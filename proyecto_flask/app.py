040f9e31b029877ef8a19434711bfc0e0d3d66fc
jeanmarcosastaciolora56-crypto <jeanmarcosastaciolora56@gmail.com>
2026-03-22 23:55:46 -0400

    Add files via upload

--- proyecto_flask/app.py
+++ proyecto_flask/app.py
@@ -5,5 +5,17 @@ app=Flask (__name__)
 def login():
     return render_template("login.html")
 
+@app.route("/recordar")
+def recordar():
+    return render_template("remember.html")
+
+@app.route("/registro")
+def registrar():
+    return render_template("sign_up.html")
+
+@app.route("/nowei", methods=['POST'])
+def mentira():
+    return render_template("loginxd.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
